b93eaf6e575489369f23e59f8d3a7e60f74ac9ba
Yatharth Kher <yatharthkhergfx@gmail.com>
2019-10-07 18:27:10 +0400

    first commit

--- src/ethereum/deploy.js
+++ src/ethereum/deploy.js
@@ -4,9 +4,9 @@ const compiledFactory=require('./build/CampaignFactory.json');
 
 const provider=new HDWalletProvider(
     //unlock wallet account
-   'tomorrow position duck measure field mention wash range book wrist coast model',
+   'page veteran drink fan host recall region stand artwork gasp initial lamp',
    //infura api to get one node for test
-   'https://rinkeby.infura.io/v3/821cce55d42c49a29f42d345f98122b9'
+   'https://rinkeby.infura.io/v3/258dc19209674c7c90614e235f08cb4a'
 );
 
 const web3=new Web3(provider);
@@ -16,7 +16,7 @@ const deploy= async ()=>{
     console.log('appempting to deploy from account', accounts[0]);
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: '0x'+compiledFactory.bytecode })
-    .send({ from: accounts[0] });
+    .send({ from: accounts[0], gas:'1000000' });
 
     console.log('contract deployed to', result.options.address);
 };
